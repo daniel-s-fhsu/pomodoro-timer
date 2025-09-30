79464e58e5d3a75372f99136a7a07c931349be9e
Daniel Seip <d_seip@mail.fhsu.edu>
2025-09-29 17:26:30 -0700

    First commit

---
+++ assets/javascript/ui.js
@@ -0,0 +1,9 @@
+  // Initialize the hamburger menu for mobile
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = null;
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+        
---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "pomodoro-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/assets/css/materialize.min.css",
+  "/assets/javascript/materialize.min.js",
+  "/assets/javascript/ui.js",
+//.  When the program code is created, cache this too
+//   "/assets/javascript/app.js",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener()
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
